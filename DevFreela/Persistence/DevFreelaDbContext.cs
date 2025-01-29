098d601c3118cb518d2ae012ecd81789e1cc8cdb
Paulo Andrade <parthur207@gmail.com>
2025-01-28 21:40:05 -0300

    .

---
+++ DevFreela/Persistence/DevFreelaDbContext.cs
@@ -0,0 +1,33 @@
+﻿using DevFreela.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevFreela.Persistence
+{
+    public class DevFreelaDbContext : DbContext
+    {
+        protected DevFreelaDbContext(DbContextOptions<DevFreelaDbContext> options)
+            : base(options)
+        {
+
+
+        }
+
+        public DbSet<Project> Projects { get; set; }
+
+        public DbSet<User> Users{ get; set; }
+
+        public DbSet<Skill> Skills { get; set; }
+
+        public DbSet<UserSkill> UserSkills { get; set; }
+
+        public DbSet<ProjectComment> ProjectComments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Skill>(x => x.HasKey(x => x.Id));
+            builder.Entity<UserSkill>(x=>x.HasKey(x=>x.))
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
